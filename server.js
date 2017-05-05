f14d3821d780fa9bf7a974e1218307b4dcca8237
Nour <nouraswad@hotmail.com>
2017-05-05 12:03:18 +0100

    Update server.js

--- server.js
+++ server.js
@@ -6,14 +6,14 @@ const bodyParser = require('body-parser');
 const config = require('./config');
 const PORT = config.PORT[process.env.NODE_ENV];
 const apiRoutes = require('./routes/api');
-//const { startMonitoring } = require('./lib/checkLate')
+const { startMonitoring } = require('./lib/checkLate')
 
 app.use(bodyParser.json());
 
 const myLogger = function (req, res, next) {
     next();
 };
-//startMonitoring();
+startMonitoring();
 
 
 app.use(myLogger);
